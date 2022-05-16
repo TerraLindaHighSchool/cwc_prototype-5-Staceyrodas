9c728d1c279ddbae439f1b3d9dad99b60ecbdb88
Staceyrodas <sr01618@srcs.org>
2022-05-16 15:42:31 -0700

    started 5.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public List<GameObject> targets2;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
