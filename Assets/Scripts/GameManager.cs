9ba4559d951a1ac6c86be55d6db8734f5872d67b
Staceyrodas <sr01618@srcs.org>
2022-05-17 15:10:28 -0700

    finished 5.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -32,7 +32,16 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets2.Count);
             Instantiate(targets2[index]);
+
+            
         }
        
     }
+
+     public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
 }
+
