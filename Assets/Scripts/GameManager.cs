0ddc72dbf0ef5e2427481e9f2d0e96f5835fd2e0
Staceyrodas <sr01618@srcs.org>
2022-05-19 15:07:49 -0700

    game 5 completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets2;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
@@ -16,22 +20,14 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+     
 
         
     }
 
 
     // Update is called once per frame
-    void Update()
-    {
-        gameOverText.gameObject.SetActive(true);
-    }
-
+  
     IEnumerator SpawnTarget()
     {
         while(isGameActive)
@@ -53,8 +49,27 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate / difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
 
